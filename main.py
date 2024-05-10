086db288fc02324ada6dd7ae7e123ba5b7087e48
Anzelah <jepkoechanzellah@gmail.com>
2024-05-10 17:16:34 +0300

    adding sound plays system sound instead of custom downloaded one. To fix

--- main.py
+++ main.py
@@ -100,10 +100,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("C:/Users/lenovo/OneDrive/Desktop/Projects/Effects/Fail.mp3", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("C:/Users/lenovo/OneDrive/Desktop/Projects/Effects/Fail.mp3", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -111,7 +113,7 @@ while True:
         playerA += 1
         pen.clear()
         pen.write("{}    {}".format(playerA, playerB), align="center", font=("Times New Roman", 25, "normal") )
-        winsound.PlaySound("Fail.mp3", winsound.SND_ASYNC)
+        #winsound.PlaySound("C:\\Users\\lenovo\\OneDrive\\Desktop\\Projects\\Effects\\Fail.mp3", winsound.SND_ASYNC)
 
     
     if ball.xcor() < -390:
@@ -120,17 +122,17 @@ while True:
         playerB += 1
         pen.clear()
         pen.write("{}    {}".format(playerA, playerB), align="center", font=("Times New Roman", 25, "normal") )
-        winsound.PlaySound("Fail.mp3", winsound.SND_ASYNC)
+        #winsound.PlaySound("C:\\Users\\lenovo\\OneDrive\\Desktop\\Projects\\Effects\\Fail.mp3", winsound.SND_ASYNC)
 
 
     # Ball hitting the paddles
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < (paddle2.ycor() +30) and ball.ycor() > (paddle2.ycor() - 30)):
-        winsound.PlaySound("./Effects/Paddle.mp3", winsound.SND_ASYNC)
+        #winsound.PlaySound("./Effects/Paddle.mp3", winsound.SND_ASYNC)
         ball.setx(340)
         ball.dx *= -1
 
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < (paddle1.ycor() +30) and ball.ycor() > (paddle1.ycor() - 30)):
-        winsound.PlaySound("./Effects/Paddle.mp3", winsound.SND_ASYNC)
+        #winsound.PlaySound("./Effects/Paddle.mp3", winsound.SND_ASYNC)
         ball.setx(-340)
         ball.dx *= -1
